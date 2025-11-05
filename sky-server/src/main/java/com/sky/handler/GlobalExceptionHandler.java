e474d84a667c7da24f24c9499728e7f189aa735e
mike <208247161@qq.com>
2025-11-05 14:59:19 +0800

    新增员工

--- sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
+++ sky-server/src/main/java/com/sky/handler/GlobalExceptionHandler.java
@@ -1,10 +1,15 @@
 package com.sky.handler;
 
+import com.sky.constant.MessageConstant;
 import com.sky.exception.BaseException;
 import com.sky.result.Result;
 import lombok.extern.slf4j.Slf4j;
+import net.bytebuddy.agent.builder.AgentBuilder;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.resource.ResourceUrlProvider;
+
+import java.sql.SQLIntegrityConstraintViolationException;
 
 /**
  * 全局异常处理器，处理项目中抛出的业务异常
@@ -12,6 +17,12 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 @Slf4j
 public class GlobalExceptionHandler {
+
+    private final ResourceUrlProvider resourceUrlProvider;
+
+    public GlobalExceptionHandler(ResourceUrlProvider resourceUrlProvider) {
+        this.resourceUrlProvider = resourceUrlProvider;
+    }
 
     /**
      * 捕获业务异常
@@ -24,4 +35,17 @@ public class GlobalExceptionHandler {
         return Result.error(ex.getMessage());
     }
 
+    @ExceptionHandler
+    public Result exceptionHandler(SQLIntegrityConstraintViolationException ex){
+        //Duplicate entry 'liss' for key 'idx_username'
+        String message = ex.getMessage();
+        if(message.contains("Duplicate entry")){
+            String[]split = message.split(" ");
+            String username=split[2];
+            String msg=username+ MessageConstant.ALREADY_EXISTS;
+            return Result.error(msg);
+        }else{
+            return Result.error(MessageConstant.UNKNOWN_ERROR);
+        }
+    }
 }
